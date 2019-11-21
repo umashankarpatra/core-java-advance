d7d71fff35103c776111ce531d0f5e6becb756aa
Umashankar Patra <upatra@XEBIAINDIA.LOCAL>
2019-11-21 13:39:40 +0530

    First commit

---
+++ src/main/java/com/uma/java8/Java8Stream.java
@@ -0,0 +1,13 @@
+package com.uma.java8;
+
+/**
+ * @author upatra
+ *
+ */
+public class Java8Stream 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Java8Stream examples" );
+    }
+}
