22eaf9fa175256c97331bc2774e9064217f980e7
umashankarpatra <umashankar.patra19@gmail.com>
2022-12-20 00:54:00 +0530

    added predicate in function argument

--- src/main/java/com/corejava/java8/PredicateExample.java
+++ src/main/java/com/corejava/java8/PredicateExample.java
@@ -3,13 +3,30 @@ package com.corejava.java8;
 import java.util.function.Predicate;
 
 public class PredicateExample {
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
+        //Simple Predicate
         // Creating predicate
-        Predicate<Integer> lesserthan = i -> (i < 45);
+        Predicate<Integer> lesserThan = i -> (i < 45);
 
         // Calling Predicate method
-        System.out.println(lesserthan.test(22));
-        System.out.println(lesserthan.test(80));
+        System.out.println(lesserThan.test(22));
+        System.out.println(lesserThan.test(80));
+
+        //Predicate chaining
+
+        Predicate<Integer> greaterThan = i -> (i > 30);
+        System.out.println("Age in between 30 to 45 :: " + lesserThan.and(greaterThan).test(40));
+
+        //Predicate as function argument
+
+        pred(100, (i) -> i <=18);
+    }
+
+    private static void pred(int number, Predicate<Integer> p) {
+        if (p.test(number)) {
+            System.out.println("Not Eligible for Vote");
+        } else {
+            System.out.println("Eligible for Vote");
+        }
     }
 }
